b766480833e24b8b54a5d0f9f626f3a31b3df781
saifpatel06 <saifradiocity@gmail.com>
2026-01-05 14:09:05 +0530

    chore: Update binary files and assembly information for AdminPanelAPI

--- Backend/obj/Debug/net10.0/AdminPanelAPI.AssemblyInfo.cs
+++ Backend/obj/Debug/net10.0/AdminPanelAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AdminPanelAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+133b1fe4871fd0c8d3278a1bfe0a424524257fae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1da9a7e5bab354e7c009b69625e1d7428d41a6f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("AdminPanelAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AdminPanelAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
